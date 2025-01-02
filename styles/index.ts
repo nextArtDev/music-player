db00c03ae0473717243c43c3be8dd7aaa9fca31c
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2025-01-02 16:56:16 +0330

    constant and initial styles

---
+++ styles/index.ts
@@ -0,0 +1,43 @@
+import { Colors, fontSize } from '@/constants/Colors'
+import { StyleSheet } from 'react-native'
+
+export const defaultStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+  },
+  text: {
+    fontSize: fontSize.base,
+    color: Colors.text,
+  },
+})
+
+export const utilsStyles = StyleSheet.create({
+  centeredRow: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  slider: {
+    height: 7,
+    borderRadius: 16,
+  },
+  itemSeparator: {
+    borderColor: Colors.textMuted,
+    borderWidth: StyleSheet.hairlineWidth,
+    opacity: 0.3,
+  },
+  emptyContentText: {
+    ...defaultStyles.text,
+    color: Colors.textMuted,
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  emptyContentImage: {
+    width: 200,
+    height: 200,
+    alignSelf: 'center',
+    marginTop: 40,
+    opacity: 0.3,
+  },
+})
