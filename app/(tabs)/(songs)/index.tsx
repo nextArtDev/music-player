d84ab2b55e2129ae72bcd99a01a827a3708d88b5
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2025-01-04 16:12:36 +0330

    search navigation

--- app/(tabs)/(songs)/index.tsx
+++ app/(tabs)/(songs)/index.tsx
@@ -1,12 +1,26 @@
 import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { defaultStyles } from '@/styles'
 import { TracksList } from '@/components/track-list'
 import library from '@/assets/data/library.json'
 import { generateTracksListId } from '@/helpers/miscellaneous'
 import { screenPadding } from '@/constants/Colors'
+import { useNavigationSearch } from '@/hooks/useNavigationSearch'
+import { trackTitleFilter } from '@/helpers/filter'
 
 const SongsScreen = () => {
+  const search = useNavigationSearch({
+    searchBarOptions: {
+      placeholder: 'Find in songs',
+    },
+  })
+
+  const filteredTracks = useMemo(() => {
+    if (!search) return library
+
+    return library.filter(trackTitleFilter(search))
+  }, [search, library])
+
   return (
     <View style={defaultStyles.container}>
       <ScrollView
@@ -14,9 +28,8 @@ const SongsScreen = () => {
         style={{ paddingHorizontal: screenPadding.horizontal }}
       >
         <TracksList
-          // id={generateTracksListId('songs', search)}
-          id={generateTracksListId('songs')}
-          tracks={library}
+          id={generateTracksListId('songs', search)}
+          tracks={filteredTracks}
           scrollEnabled={false}
         />
       </ScrollView>
