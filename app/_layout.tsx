bc38ca728dc0653096db6411482780c687cf7fb9
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2025-01-04 16:38:45 +0330

    setup track player

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,25 +7,38 @@ import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
 import { StatusBar } from 'expo-status-bar'
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 import 'react-native-reanimated'
 import '../global.css'
 import { useColorScheme } from '@/hooks/useColorScheme'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import TrackPlayer from 'react-native-track-player'
+import { useSetupTrackPlayer } from '@/hooks/useSetupTrackPlayer'
+import { useLogTrackPlayerState } from '@/hooks/useLogTrackPlayerState'
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
 
+// TrackPlayer.registerPlaybackService(() => playbackService)
 export default function RootLayout() {
   const colorScheme = useColorScheme()
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   })
-
+  const handleTrackPlayerLoaded = useCallback(() => {
+    SplashScreen.hideAsync()
+  }, [])
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync()
     }
   }, [loaded])
+
+  useSetupTrackPlayer({
+    onLoad: handleTrackPlayerLoaded,
+  })
+
+  useLogTrackPlayerState()
 
   if (!loaded) {
     return null
